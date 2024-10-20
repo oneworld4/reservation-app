03aef0372d93f539120934212b8e8ad3832ebe0b
Ishibashi <yu0201ishibashi@yahoo.co.jp>
2024-10-20 21:44:00 +0900

    ヘッダーコンポーネント追加

---
+++ src/app/common/navbar/navbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss'
+})
+export class NavbarComponent {
+
+}
